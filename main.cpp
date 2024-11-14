e2855b392ecc078dc0d3066899c76bf5dc63adab
Vincent-Vargas-12 <vincentvargas12@gmail.com>
2024-11-13 18:52:44 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,118 +2,97 @@
 #include <string> //data to fill contailer
 #include <vector> //container for set pieces
 #include <cstdlib> //used for clearing the terminal
-#include <chrono> //used to allow messages to display for a short amount of time
-#include <thread> //
-
-void print_row(std::vector<std::string> row) {
-  for(int i = 0; i < row.size(); i++){
-  std::cout << "|" <<row.at(i) << "|";
-  }
-  std::cout <<"\n";
-}
-
-void insert_token(std::vector<std::string> row, int column_number, std::string token_piece) {
-  row.erase(row.begin() + column_number);
-  row.insert(row.begin() + column_number, token_piece);
-}
 
 int main() {
-  std::vector<std::string> row_1,row_2,row_3,row_4,row_5,row_6;
-  for(int i = 0; i < 7; i++){
-    row_1.push_back(" - ");
+  int column = 7;
+  int row = 6;
+
+  //Self explanatory variables. decidedes how many rows and columns the board will have. 
+
+  std::string board[row][column]; //initalization of board
+
+  for (int i = 0; i < row; i++) {         //fill board with "white space" or "blank spaces";
+    for (int j = 0; j < column; j++) {
+      board[i][j] = " - ";
+    }
   }
 
-  for(int i = 0; i < 7; i++){
-    row_2.push_back(" - ");
+  for (int i = 0; i < row; i++) {          //print board
+        for (int j = 0; j < column; j++) { 
+            std::cout << board[i][j]; 
+        }
+        std::cout << "\n"; 
+        int column_num = 1;
+      if(i == row - 1){
+        for(int k = 0; k < column; k++){
+          std::cout << " " << column_num << " ";
+          column_num++;
+        }
+        std::cout << "\n";
+      }
   }
-
-  for(int i = 0; i < 7; i++){
-    row_3.push_back(" - ");
-  }
-
-  for(int i = 0; i < 7; i++){
-    row_4.push_back(" - ");
-  }
-
-  for(int i = 0; i < 7; i++){
-    row_5.push_back(" - ");
-  }
-
-  for(int i = 0; i < 7; i++){
-    row_6.push_back(" - ");
-  }
-  system("clear");
-  print_row(row_6);
-  print_row(row_5);
-  print_row(row_4);
-  print_row(row_3);
-  print_row(row_2);
-  print_row(row_1);
-  std::cout << "  1    2    3    4    5    6    7\n";
   
-  int swap_counter = 0;
-  int column_number;
-  std::string token;
-  std::string Token_One = " X ";
-  std::string Token_Two = " O ";
+  int swap_counter = 0; //counter that counts how many tokens have been places. currently used to end the game, but will 
+  //determine ties in the future
+  int column_number; //variable player uses to decide which column to pick
+  std::string token;  //string that determines which token will be used
+  std::string Token_One = " X ";  //player one token, " X " is a place holder for now
+  std::string Token_Two = " O ";  //same as above, but for player 2 and also a place holder;
 
-  while (swap_counter != 42) {
+
+  while (swap_counter != row * column) { //when swap counter is equal to the number of rows and columns, the game is over since there are no more tokens at play
+    int row_minus_one = row - 1;  //used for the zero-based arrya
+    int row_counter = 0; //used to naviate through the rows
     std::cout << "Enter a valid column number \n";
     std::cin >> column_number;
-      while(column_number > 7 || column_number < 0){
+      while(column_number > column || column_number < 0){  //ensure that the player can't pick a column that doesn't exist. forces them to pick a column;
         std::cout << "Invalid Number. Please enter a valid column number \n";
         std::cin >> column_number;
       }
-      int column_number_minus = column_number - 1;
       
-     
-      if (swap_counter % 2 == 0){
-        token = Token_One;
-      } else {
+      system("clear"); //clears the terminal to display key info (board, text, etc.)
+
+      if (swap_counter % 2 == 0){  //since there is only two players,
+        token = Token_One;         //we can use % to alternate between
+      } else {                     //player 1 and player 2 tokens
         token = Token_Two;
       }
+      
+      if(board[0][column_number - 1] == Token_One || board[0][column_number - 1] == Token_Two) { //logic to make sure the player the token can be placed in a valid spot
 
-      if(column_number >= 1 && column_number <= 7) {
-          if (row_6.at(column_number - 1) == Token_One || row_6.at(column_number - 1) == Token_Two){ 
-          std::cout << "Column is full. Please pick a different column number \n";
-          std::this_thread::sleep_for(std::chrono::seconds(3));
-
-          } else if (row_5.at(column_number - 1) == Token_One || row_5.at(column_number - 1) == Token_Two) {
-                row_6.erase(row_6.begin() + column_number_minus);
-                row_6.insert(row_6.begin() + column_number_minus, token);
-                swap_counter++;
-          } else if(row_4.at(column_number - 1) == Token_One || row_4.at(column_number - 1) == Token_Two) {
-                row_5.erase(row_5.begin() + column_number_minus);
-                row_5.insert(row_5.begin() + column_number_minus, token);
-                swap_counter++;
-          } else if(row_3.at(column_number - 1) == Token_One || row_3.at(column_number - 1) == Token_Two) {
-                row_4.erase(row_4.begin() + column_number_minus);
-                row_4.insert(row_4.begin() + column_number_minus, token);
-                swap_counter++;
-          } else if(row_2.at(column_number - 1) == Token_One || row_2.at(column_number - 1) == Token_Two) {
-                row_3.erase(row_3.begin() + column_number_minus);
-                row_3.insert(row_3.begin() + column_number_minus, token);
-                swap_counter++;
-          } else if(row_1.at(column_number - 1) == Token_One || row_1.at(column_number - 1) == Token_Two) {
-                row_2.erase(row_2.begin() + column_number_minus);
-                row_2.insert(row_2.begin() + column_number_minus, token);
-                swap_counter++;
-          } else {
-                row_1.erase(row_1.begin() + column_number_minus);
-                row_1.insert(row_1.begin() + column_number_minus, token);
-                swap_counter++;
+        std::cout << "Column " << column_number << " is full. Please Select a different column \n"; //ensures player can't replace top value
+        swap_counter--;
+      } else {
+        while(board[row_counter][column_number - 1] != Token_One || board[row_counter][column_number - 1] != Token_Two){
+        if(row_counter == row_minus_one) { //ensure the while loop does not run forever by placing the last token at the bottom of the row;
+          board[row - 1][column_number - 1] = token;
+          break;
+        } else {
+          if(board[row_counter + 1][column_number -1] == Token_One || board[row_counter + 1][column_number - 1] == Token_Two){ //checks one space below
+          board[row_counter][column_number - 1] = token; //places token on top the token below it.
+          break;
           }
-    }
-  
-  system("clear");
-  print_row(row_6);
-  print_row(row_5);
-  print_row(row_4);
-  print_row(row_3);
-  print_row(row_2);
-  print_row(row_1);
-  std::cout << "  1    2    3    4    5    6    7\n";
+          row_counter++;
+     }
+        }
+      }
 
-  std::cout << "There are " << 42 - swap_counter << " tokens left\n";
+  
+    for (int i = 0; i < row; i++) {          //prints board
+        for (int j = 0; j < column; j++) { 
+            std::cout << board[i][j]; 
+        }
+        std::cout << "\n"; 
+        int column_num = 1;
+      if(i == row - 1){
+        for(int k = 0; k < column; k++){
+          std::cout << " " << column_num << " ";
+          column_num++;
+        }
+        std::cout << "\n";
+      }
+  }
+  swap_counter++;
+  std::cout << "There are " << (row * column) - swap_counter << " tokens left\n";
   }
 }
